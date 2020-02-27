6d9a0ef2014443dad7108f8c66347340b55adf0a
Traian Svinti <traian.svinti@gmail.com>
2020-02-27 11:20:05 +0000

    Small improvements

--- app-hotkey.sh
+++ app-hotkey.sh
@@ -7,6 +7,10 @@ if [[ -z "$APP" ]]; then  # Check if command has an app
 else
     if [[ -n $(pgrep "${APP}") ]]; then  # Check if app is running
         wmctrl -i -a $(wmctrl -lp | tr '[:upper:]' '[:lower:]' | grep "${APP}" | cut '-d ' -f1)  # Bring to front
+
+        if [[ $? != 0 ]]; then
+            wmctrl -i -a $(wmctrl -lx | tr '[:upper:]' '[:lower:]' | grep "${APP}" | cut '-d ' -f1)  # Bring to front
+        fi
     else
         $(compgen -c | grep $APP | head -n 1) > /dev/null  # Start app
     fi
