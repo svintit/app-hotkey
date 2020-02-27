fa44f119b1830179ff9ff2f47b04ca6380a8d8ad
Traian Svinti <traian.svinti@gmail.com>
2020-02-27 11:09:44 +0000

    Small change to check if app is already running

--- app-hotkey.sh
+++ app-hotkey.sh
@@ -5,8 +5,8 @@ APP=$1
 if [[ -z "$APP" ]]; then  # Check if command has an app
     printf "\n--> Please follow the command with an application name \n"
 else
-    if [[ -n $(ps -aux | grep $APP) ]]; then  # Check if app is running
-        wmctrl -i -a $(wmctrl -lx | tr '[:upper:]' '[:lower:]' | grep "${APP}" | cut '-d ' -f1)  # Bring to front
+    if [[ -n $(pgrep "${APP}") ]]; then  # Check if app is running
+        wmctrl -i -a $(wmctrl -lp | tr '[:upper:]' '[:lower:]' | grep "${APP}" | cut '-d ' -f1)  # Bring to front
     else
         $(compgen -c | grep $APP | head -n 1) > /dev/null  # Start app
     fi
