a818133c71244edc228f0f1f3534ae0bfd20db52
Traian Svinti <traian.svinti@gmail.com>
2020-02-27 11:27:57 +0000

    Small improvements

--- app-hotkey.sh
+++ app-hotkey.sh
@@ -2,16 +2,23 @@
 
 APP=$1
 
+function bring-to-front {
+    wmctrl -i -a $(wmctrl -lp | tr '[:upper:]' '[:lower:]' | grep "${APP}" | cut '-d ' -f1)
+
+    if [[ $? != 0 ]]; then
+        wmctrl -i -a $(wmctrl -lx | tr '[:upper:]' '[:lower:]' | grep "${APP}" | cut '-d ' -f1)
+    fi
+}
+
 if [[ -z "$APP" ]]; then  # Check if command has an app
     printf "\n--> Please follow the command with an application name \n"
 else
     if [[ -n $(pgrep "${APP}") ]]; then  # Check if app is running
-        wmctrl -i -a $(wmctrl -lp | tr '[:upper:]' '[:lower:]' | grep "${APP}" | cut '-d ' -f1)  # Bring to front
-
-        if [[ $? != 0 ]]; then
-            wmctrl -i -a $(wmctrl -lx | tr '[:upper:]' '[:lower:]' | grep "${APP}" | cut '-d ' -f1)  # Bring to front
-        fi
+        bring-to-front
+    elif [[ -n $(ps -aux | grep "${APP}") ]]; then
+        bring-to-front
     else
+        notify-send "Opening '${APP}'"
         $(compgen -c | grep $APP | head -n 1) > /dev/null  # Start app
     fi
 fi
